011f6edf34fa7bdbc9f719b4bb1a029b5eb9cc5a
Rohit Nepali <rohitnepali75@gmail.com>
2024-06-10 18:32:56 +0545

    First commit database connected

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using CRUDapplication.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDapplication.Data
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) 
+        {
+            
+        }
+
+        //DB Context connects the sqlServer to this application
+        public DbSet<User> Teachers { get; set; }
+        public DbSet<User> Students { get; set; }
+    }
+}
